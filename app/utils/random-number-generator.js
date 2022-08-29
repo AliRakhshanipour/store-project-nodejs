d5f9251a7b8abcbb5374c270063bd068da8d9c77
Ali Rakhshanipur <alirakhshanipur@Alis-MacBook-Pro.local>
2022-08-29 10:52:53 +0430

    get-otp , check-opt , create access-token , check access-token

---
+++ app/utils/random-number-generator.js
@@ -0,0 +1,7 @@
+const randomNumberGenerator = () => {
+  return Math.floor(Math.random() * 9000 + 10000);
+};
+
+module.exports = {
+  randomNumberGenerator,
+};
---
+++ app/utils/token-generator.js
@@ -0,0 +1,25 @@
+const createHttpError = require("http-errors");
+const JWT = require("jsonwebtoken");
+const { UserModel } = require("../models/user.model");
+const { ACCESS_TOKEN_SECRET_KEY } = require("./constants");
+
+const signAccessToken = (userId) => {
+  return new Promise(async (resolve, reject) => {
+    const user = await UserModel.findById({ _id: userId });
+    const payLoad = {
+      phone: user.phone,
+    };
+    const secretKey = ACCESS_TOKEN_SECRET_KEY;
+    const option = {
+      expiresIn: "1h",
+    };
+    JWT.sign(payLoad, secretKey, option, (error, token) => {
+      if (error) reject(createHttpError.InternalServerError("Server Error"));
+      resolve(token);
+    });
+  });
+};
+
+module.exports = {
+  signAccessToken,
+};
